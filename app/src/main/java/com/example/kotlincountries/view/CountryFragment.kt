887858768e0e260e9be424a319aff7ec34dfccd2
Coola <ygtkula16@gmail.com>
2022-08-04 13:35:39 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/kotlincountries/view/CountryFragment.kt
@@ -0,0 +1,48 @@
+package com.example.kotlincountries.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kotlincountries.CountryFragmentArgs
+import com.example.kotlincountries.R
+
+
+class CountryFragment : Fragment() {
+
+
+    private var countryUuid = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            countryUuid = CountryFragmentArgs.fromBundle(it).countryUuid
+        }
+    }
+
+}
+
+/*
+        Action Atama
+       fragment_button.setOnClickListener{
+           val action = FeedFragmentDirections.actionFeedFragmentToCountryFragment()
+           Navigation.findNavController(it).navigate(action)
+       }
+
+        */
