33b4a5d307c6b80dabc93719e7846324798959c0
Coola <ygtkula16@gmail.com>
2022-08-05 11:07:11 +0300

    Cleaned UI code

--- app/src/main/java/com/example/kotlincountries/view/FeedFragment.kt
+++ app/src/main/java/com/example/kotlincountries/view/FeedFragment.kt
@@ -5,8 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlincountries.R
 import com.example.kotlincountries.adapter.CountryAdapter
@@ -15,13 +14,13 @@ import kotlinx.android.synthetic.main.fragment_feed.*
 
 
 class FeedFragment : Fragment() {
-    private lateinit var viewModel: FeedViewModel
+
+    private val viewModel: FeedViewModel by viewModels()
     private val countryAdapter = CountryAdapter(arrayListOf())
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -36,8 +35,6 @@ class FeedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(FeedViewModel::class.java)
-        //viewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java))
         viewModel.refreshData()
 
         countryList.layoutManager = LinearLayoutManager(context)
@@ -46,35 +43,28 @@ class FeedFragment : Fragment() {
     }
 
     fun observeLiveData() {
-        viewModel.countries.observe(viewLifecycleOwner, Observer { countries ->
-            countries?.let {
-                countryList.visibility = View.VISIBLE
-                countryAdapter.updateCountryList(countries)
-            }
-        })
-        viewModel.countryError.observe(viewLifecycleOwner, Observer { error ->
-            error?.let {
-                if (it) {
-                    countryError.visibility = View.VISIBLE
-                } else {
-                    countryError.visibility = View.GONE
-                }
-            }
-        })
-        /*
-        viewModel.countryLoading.observe(viewLifecycleOwner, Observer { loading->
-            loading?.let{
-                if(it){
-                    countryLoading.visibility = View.VISIBLE
-                    countryList.visibility = View.GONE
-                    countryList.visibility = View.GONE
-                }
-                else{
-                     countryLoading.visibility = View.GONE
-                }
-            }
-        })
-        */
 
+        viewModel.countries.observe(viewLifecycleOwner) { countries ->
+            countryList.visibility = View.VISIBLE
+            countryAdapter.updateCountryList(countries)
+        }
+
+        viewModel.countryError.observe(viewLifecycleOwner) { error ->
+            if (error) {
+                countryError.visibility = View.VISIBLE
+            } else {
+                countryError.visibility = View.GONE
+            }
+        }
+
+        viewModel.countryLoading.observe(viewLifecycleOwner) { loading ->
+            if (loading) {
+                progressBar.visibility = View.VISIBLE
+                countryList.visibility = View.GONE
+            } else {
+                progressBar.visibility = View.GONE
+                countryList.visibility = View.VISIBLE
+            }
+        }
     }
 }
